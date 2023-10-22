c7c4e0d1aa1d057d764affa8fa17248f62745b36
Luiz Guilherme <luizgmelo64@gmail.com>
2023-10-22 12:00:54 -0300

    modified for showTenPokemon

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-async function ShowPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon-form/25')
-    const pokemon = await response.json()
-    console.log(pokemon.name)
-    console.log(pokemon.sprites.front_default)
-    document.body.innerHTML += `<div>
-    <p>${pokemon.name}</p>
-    <img src='${pokemon.sprites.front_default}'></img>
-    </div>`
+async function ShowTenPokemon() {
+    for (let i = 1; i < 11; i++) {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${i}`)
+        const pokemon = await response.json()
+        console.log(pokemon.name)
+        console.log(pokemon.sprites.front_default)
+    
+        const pokedex = document.querySelector('.pokedex');
+
+        const card = document.createElement('div')
+        card.className = 'pokemon-card'
+
+
+        const name = document.createElement('p');
+        const node = document.createTextNode(pokemon.name);
+        name.appendChild(node);
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', pokemon.sprites.front_default)
+        
+        card.appendChild(name)
+        card.appendChild(img)
+        pokedex.appendChild(card)
+    }
+   
 }
 
-ShowPokemon()
+ShowTenPokemon()
